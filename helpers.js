73719e7604e9a55de3f2f2f5cfa1159010ab17e7
0xMonnn <imankurniawan1933@gmail.com>
2025-02-19 01:52:08 +0700

    helpers.js

--- helpers.js
+++ helpers.js
@@ -4,14 +4,14 @@ async function delay(ms) {
 
 function displayHeader() {
   process.stdout.write("\x1Bc");
-  console.log(chalk.cyan.bold(` █████╗ ███████╗██╗   ██╗███╗   ██╗ ██████╗`));
-  console.log(chalk.cyan.bold(`██╔══██╗██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝`));
-  console.log(chalk.cyan.bold(`███████║███████╗ ╚████╔╝ ██╔██╗ ██║██║      `));
-  console.log(chalk.cyan.bold(`██╔══██║╚════██║  ╚██╔╝  ██║╚██╗██║██║      `));
-  console.log(chalk.cyan.bold(`██║  ██║███████║   ██║   ██║ ╚████║╚██████╔ `));
-  console.log(chalk.cyan.bold(`╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝ `));
-  console.log(chalk.cyan.bold(`               JOIN AIRDROP         `));
-  console.log(chalk.cyan.bold(`      https://t.me/AirdropSyncReborn `));
+  console.log(` █████╗ ███████╗██╗   ██╗███╗   ██╗ ██████╗`));
+  console.log(`██╔══██╗██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝`));
+  console.log(`███████║███████╗ ╚████╔╝ ██╔██╗ ██║██║      `));
+  console.log(`██╔══██║╚════██║  ╚██╔╝  ██║╚██╗██║██║      `));
+  console.log(`██║  ██║███████║   ██║   ██║ ╚████║╚██████╔ `));
+  console.log(`╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝ `));
+  console.log(`               JOIN AIRDROP         `));
+  console.log(`      https://t.me/AirdropSyncReborn `));
 }
 
 module.exports = {
